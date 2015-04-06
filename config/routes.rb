2394f06a39901f1c4cd8c3b3f55882a256e92a10
Corbin Page <corbin.page@gmail.com>
2015-04-06 19:41:00 -0400

    Initial copy

---
+++ config/routes.rb
@@ -0,0 +1,49 @@
+Rails.application.routes.draw do
+  root to: "home#index"
+  get '/portal', to: 'users#portal', as: 'portal'
+
+  # Client Methods
+  devise_for :users, :controllers => { :registrations => "users/registrations", :sessions => "users/sessions" }
+  # resources :users do
+  #   resources :jobs, only: [:new, :create, :show, :update, :index]
+  # end
+
+  resources :procedures
+  # get   '/procedure/:id',         to: 'procedures#show', as: 'procedure_show'
+
+  # resources :consents
+  get '/consents', to: 'consents#index', as: 'consents'
+
+  get '/procedure/:procedure_id/consent/create', to: 'consents#create', as: 'create_consent'
+  get '/procedure/:procedure_id/consent/:consent_id', to: 'consents#show', as: 'show_consent'
+  get '/procedure/:id/consent/:consent_id/update', to: 'consents#update', as: 'update_consent'
+
+  get '/procedure/:procedure_id/consent/new', to: 'consents#new', as: 'consent_new'
+  get '/procedure/:procedure_id/consent/:consent_id/indications', to: 'consents#indications', as: 'consent_indications'
+  get '/procedure/:procedure_id/consent/:consent_id/benefits', to: 'consents#benefits', as: 'consent_benefits'
+  get '/procedure/:procedure_id/consent/:consent_id/risks', to: 'consents#risks', as: 'consent_risks'
+  get '/procedure/:procedure_id/consent/:consent_id/alternatives', to: 'consents#alternatives', as: 'consent_alternatives'
+  get '/procedure/:procedure_id/consent/:consent_id/description', to: 'consents#description', as: 'consent_description'
+
+  post '/contact_us',            to: 'messages#contact_us',            as: 'contact_us'
+  # post '/mailgun_receive_email', to: 'messages#mailgun_receive_email', as: 'mailgun_receive_email'
+
+  # Proofreader methods
+  resources :users, only: [:index]
+  resources :messages, only: [:index]
+  get   "/users/:id/manage",            to: 'users#manage',            as: "manage_user"
+  patch "/users/:id/management_update", to: 'users#management_update', as: "user_management_update"
+
+  # get   'all_jobs', to: 'jobs#all_jobs', as: 'all_jobs'
+  # get   'jobs/:id/manage',         to: 'jobs#manage', as: 'job_manage'
+  # patch 'jobs/:id/manage_update',  to: 'jobs#manage_update'
+  # post  'tasks/:id/start',         to: 'tasks#create_timer'
+  # patch 'timers/:id/end',          to: 'timers#end_timer', as: 'end_timer'
+  # patch 'tasks/:id/upload',        to: 'tasks#upload_files' # Is this a post route, Only file uploaded as sub element to task
+  # patch 'tasks/:id/mark_complete', to: 'tasks#mark_complete'
+  # patch 'jobs/:id/proofreader_message', to: 'jobs#send_client_email'
+
+  # resources :charges, :only => [:new]
+  # post '/buy_credits', to: 'charges#buy_credits', as: 'buy_credits'
+  
+end
---
+++ db/schema.rb
@@ -0,0 +1,138 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150214182953) do
+
+  create_table "attachments", force: true do |t|
+    t.integer  "message_id"
+    t.integer  "task_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
+  end
+
+  add_index "attachments", ["message_id"], name: "index_attachments_on_message_id"
+  add_index "attachments", ["task_id"], name: "index_attachments_on_task_id"
+
+  create_table "consents", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "procedure_id"
+    t.boolean  "indications_complete",  default: false
+    t.boolean  "benefits_complete",     default: false
+    t.boolean  "risks_complete",        default: false
+    t.boolean  "alternatives_complete", default: false
+    t.boolean  "description_complete",  default: false
+    t.boolean  "consent_complete",      default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "jobs", force: true do |t|
+    t.integer  "user_id"
+    t.string   "job_type"
+    t.boolean  "complete"
+    t.datetime "completed_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.float    "total_time"
+  end
+
+  add_index "jobs", ["job_type"], name: "index_jobs_on_job_type"
+  add_index "jobs", ["user_id"], name: "index_jobs_on_user_id"
+
+  create_table "messages", force: true do |t|
+    t.integer  "job_id"
+    t.string   "name"
+    t.string   "email"
+    t.string   "telephone"
+    t.string   "subject"
+    t.text     "body"
+    t.string   "location_submitted"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "mailgun_id"
+    t.string   "mailgun_reply_to_id"
+    t.string   "mailgun_response"
+  end
+
+  add_index "messages", ["job_id"], name: "index_messages_on_job_id"
+  add_index "messages", ["mailgun_id"], name: "index_messages_on_mailgun_id"
+  add_index "messages", ["mailgun_reply_to_id"], name: "index_messages_on_mailgun_reply_to_id"
+
+  create_table "procedure_details", force: true do |t|
+    t.integer  "procedure_id"
+    t.text     "indications_text"
+    t.text     "benefits_text"
+    t.text     "risks_text"
+    t.text     "alternatives_text"
+    t.text     "description_text"
+    t.text     "post_op_text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "procedures", force: true do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.integer "job_id"
+    t.string  "task_type"
+    t.boolean "complete"
+  end
+
+  add_index "tasks", ["job_id"], name: "index_tasks_on_job_id"
+  add_index "tasks", ["task_type"], name: "index_tasks_on_task_type"
+
+  create_table "timers", force: true do |t|
+    t.integer  "task_id"
+    t.integer  "proofreader_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+  end
+
+  add_index "timers", ["proofreader_id"], name: "index_timers_on_proofreader_id"
+  add_index "timers", ["task_id"], name: "index_timers_on_task_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.string   "telephone"
+    t.text     "internal_bio"
+    t.integer  "role",                   default: 0
+    t.integer  "credits",                default: 0
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["role"], name: "index_users_on_role"
+
+end
