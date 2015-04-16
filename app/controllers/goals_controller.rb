c2a96c47609370f2807ab68103b4d295acae3991
Corbin Page <corbin.page@gmail.com>
2015-04-16 19:20:20 -0400

    Fixed goals_controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,18 +3,18 @@ class GoalsController < ApplicationController
   def update_day
     @goal = Goal.find_one(params[:id])
 
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.js   {}
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
 
- respond_to do |format|
-    if @user.save
-      format.html { redirect_to @user, notice: 'User was successfully created.' }
-      format.js   {}
-      format.json { render json: @user, status: :created, location: @user }
-    else
-      format.html { render action: "new" }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+
     end
-
-
   end
 
 end
