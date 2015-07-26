46c46ff4b84c358ba706615ac6e2f688a5647bb7
Corbin Page <corbin.page@gmail.com>
2015-07-26 16:56:34 -0400

    Progresses added and migration complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-users = [ {email: "surgentt@gmail.com",     name: "Thomas Surgent"}, 
-          {email: "corbin.page@gmail.com",  name: "Corbin Page"}
+users = [ {email: "corbin.page@gmail.com",  name: "Corbin Page"},
+  {email: "surgentt@gmail.com",     name: "Thomas Surgent"}
 ]
 users.each do |user|
   user = User.new(
@@ -10,14 +10,24 @@ users.each do |user|
   user.save
 end
 
-goals = [{name: "Workout",  category: "Health", theme: "habeats-blue", user_id: 2}, 
- {name: "Eating",  category: "Health", theme: "habeats-green", user_id: 2}, 
- {name: "Code",  category: "Work", theme: "habeats-yellow", user_id: 2} 
-]
-
-goals.each do |g|
-  goal_obj = Goal.new(g)
-  goal_obj.initialize_progresses
-  # goal_obj.initialize_empty_progresses
-  goal_obj.save
+new_goal = Goal.new({name: "7min",  category: "Health", theme: "habeats-blue", user_id: 1})
+new_goal.save!
+file = File.read('db/seven-export.json')
+day_hash = JSON.parse(file)
+# binding.pry
+new_goal.progresses = day_hash.map do |d| 
+  Progress.create(new_goal.id,d["date"],d["score"])
 end
+new_goal.save!
+
+# goals = [{name: "Workout",  category: "Health", theme: "habeats-blue", user_id: 1}, 
+#  {name: "Eating",  category: "Health", theme: "habeats-green", user_id: 1}, 
+#  {name: "Code",  category: "Work", theme: "habeats-yellow", user_id: 1} 
+# ]
+
+# goals.each do |g|
+#   goal_obj = Goal.new(g)
+#   goal_obj.initialize_progresses
+#   # goal_obj.initialize_empty_progresses
+#   goal_obj.save
+# end
